9837e5d6ee46bdf99c258d3e377216bfc9d6d793
stonedunlaptm2 <tstone112307@gmail.com>
2024-10-08 17:04:54 -0500

    complete with changes needed

--- scripts/movies_database.sql
+++ scripts/movies_database.sql
@@ -34,12 +34,21 @@ LEFT JOIN revenue as r
 ON s.movie_id = r.movie_id
 WHERE s.mpaa_rating = 'G'
 ORDER BY r.worldwide_gross DESC
-
 --Answer: Toy Story 4
 
 --Which company distributed it?
 
-
+SELECT 
+s.movie_id,
+s.film_title,
+s.mpaa_rating,
+s.domestic_distributor_id,
+d.distributor_id
+FROM distributors as d
+LEFT JOIN specs as s
+ON d.distributor_id=s.domestic_distributor_id
+WHERE s.mpaa_rating = 'G'
+--Answer: Walt Disney
 
 --4. Write a query that returns, for each distributor in the distributors table, the distributor name and the number of movies associated with that distributor in the movies 
 table. Your result set should include all of the distributors, whether or not they have any movies in the movies table.
@@ -51,18 +60,40 @@ SELECT
 FROM distributors as d
 FULL JOIN specs as s
 ON d.distributor_id=s.domestic_distributor_id;
-
+--Answer- Complete
 
 --5. Write a query that returns the five distributors with the highest average movie budget.
 SELECT 
 r.movie_id,
-r.film_budget
-d.distributor_id
-FROM revenue as r
-INNER JOIN distributor as d
-ON r.movie_id = d.distributor_id
+r.film_budget,
+d.distributor_id,
+r.worldwide_gross
+FROM distributor as d
+LEFT JOIN revenue as r
+ON d.distributor_id AND r.movie_id 
 ORDER BY film_budget DESC
 LIMIT 5;
 
 --6. How many movies in the dataset are distributed by a company which is not headquartered in California? Which of these movies has the highest imdb rating?
+SELECT 
+d.company_name,
+d.headquarters,
+r.imdb_rating
+FROM distributors as d
+LEFT JOIN rating as r
+ON d.distributor_id=r.movie_id
+WHERE d.headquarters NOT LIKE 'CA'
+ORDER BY r.imdb_rating ASC;
+
 --7. Which have a higher average rating, movies which are over two hours long or movies which are under two hours?
+SELECT
+    s.movie_id,
+    s.film_title,
+    s.length_in_min,
+    AVG(r.imdb_rating) AS avg_rating
+FROM specs AS s
+LEFT JOIN rating AS r 
+ON s.movie_id = r.movie_id
+GROUP BY s.movie_id, s.film_title, s.length_in_min, r.imdb_rating
+ORDER BY avg_rating DESC;
+--Answer- There is a tie for the average of both types
